eaf3c8abee0d5964ff4dc0c8e12448fa55ec05fd
xanqus <sbk98031816@gmail.com>
2021-03-04 18:33:24 +0900

    v 1

---
+++ src/components/NewsList.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import axios from 'axios';
+import usePromise from './../lib/usePromise';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const NewsList = ({ category }) => {
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=353ce0aa0fe34345a53c8827fc5dde2d`,
+    );
+  }, [category]);
+
+  if (loading) {
+    return <NewsListBlock>대기 중...</NewsListBlock>;
+  }
+  if (!response) {
+    return null;
+  }
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
+
+  const { articles } = response.data;
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article}></NewsItem>
+      ))}
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
